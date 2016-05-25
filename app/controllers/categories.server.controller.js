0d8b32433cf63002d337fb1fd60be007f082fc4e
ahmerlums <17100033@lums.edu.pk>
2016-05-25 19:16:21 +0500

    Added Categories Model with Create and Index operations!

--- app/controllers/categories.server.controller.js
+++ app/controllers/categories.server.controller.js
@@ -4,13 +4,24 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+    Category = mongoose.model('Category'),
     _ = require('lodash');
 
 /**
  * Create a Category
  */
 exports.create = function(req, res) {
-
+	var category = new Category(req.body);
+	category.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.status(201).json(category);
+		}
+	});
 };
 
 /**
@@ -38,5 +49,14 @@ exports.delete = function(req, res) {
  * List of Categories
  */
 exports.list = function(req, res) {
+	Category.find().exec(function(err, categories) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.json(categories);
+        }
+    });
 
 };
---
+++ app/routes/categories.server.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app) {
+	// Routing logic  
+	var categories = require('../../app/controllers/categories.server.controller');
+
+	app.route('/categories')
+	  .get(categories.list)
+	  .post(categories.create);
+};
